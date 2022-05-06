5e2f3f6dfd30a86840977851711fbba342c0bbbe
franklin rodriguez <franklinserif@gmail.com>
2022-05-05 21:15:06 -0400

    react router dom app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
