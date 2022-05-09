1c61636e9a407c62fdc0e0fdd0b084d27e86a8b0
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 20:11:00 -0400

    basic routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import InitSession from './layout/InitSession';
+import Layout from './layout/Layout';
+import Index from './pages/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<InitSession />}></Route>
+        <Route path="/clientes" element={<Layout />}>
+          <Route element={<Index />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
