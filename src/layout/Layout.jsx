0a02415063069e2c9a4f8da8924ed9091903b538
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 21:40:42 -0400

    new client css

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -42,7 +42,7 @@ const Layout = () => (
         </NavLink>
       </nav>
     </div>
-    <div className="md:w-3/4">
+    <div className="md:w-3/4 p-10">
       <Outlet />
     </div>
   </div>
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -3,7 +3,13 @@
  * @returns {JSX.Element} <NewClient/>
  */
 const NewClient = () => {
-  return <div>NewClient</div>;
+  return (
+    <>
+      <h1 className="font-black text-3xl text-blue-400 font-normal">
+        Nuevo Cliente
+      </h1>
+    </>
+  );
 };
 
 export default NewClient;
