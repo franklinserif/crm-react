adca005e36fe0b820fdd42b147fb1fc6d187cada
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 20:32:20 -0400

    remove imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import InitSession from './layout/InitSession';
 import Layout from './layout/Layout';
 import Index from './pages/Index';
-import LoginForm from './pages/LoginForm';
 import NewClient from './pages/NewClient';
 import EditClient from './pages/EditClient';
 
