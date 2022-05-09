0df8da677c4dd45a72832f5b0ed16d2231e87af1
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 21:25:25 -0400

    Change a to Link from router dom

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 /**
  * Layout for /Clients routes
@@ -9,18 +9,18 @@ const Layout = () => (
     <div className="md:w-1/4 bg-gray-200 px-5 py-10">
       <h2 className="text-3xl text-center text-gray-500">CRM - Clientes</h2>
       <nav className="mt-10">
-        <a
+        <Link
           className="text-gray-600 text-1xl block uppercase mt-2 hover:text-gray-900 transition duration-150 ease-out hover:ease-in"
-          href="/clientes"
+          to="/clientes"
         >
           Clientes
-        </a>
-        <a
+        </Link>
+        <Link
           className="text-gray-600 text-1xl block uppercase mt-2 hover:text-gray-900 transition duration-150 ease-out hover:ease-in"
-          href="/clientes/nuevo"
+          to="/clientes/nuevo"
         >
           Nuevo Cliente
-        </a>
+        </Link>
       </nav>
     </div>
     <div className="md:w-3/4">
