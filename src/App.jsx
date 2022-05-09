96ed6f1809728170a5d90e6699e87f80c78863fb
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 20:15:44 -0400

    New page LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InitSession from './layout/InitSession';
 import Layout from './layout/Layout';
 import Index from './pages/Index';
+import LoginForm from './pages/LoginForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<InitSession />}></Route>
+        <Route path="/" element={<InitSession />}>
+          <Route index element={<LoginForm />} />
+        </Route>
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Index />} />
         </Route>
