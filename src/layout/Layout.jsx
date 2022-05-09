766288b613593ee59441410c7195cb14cf05e130
franklin rodriguez <franklinserif@gmail.com>
2022-05-08 21:22:37 -0400

    css style layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,15 +4,28 @@ import { Outlet } from 'react-router-dom';
  * Layout for /Clients routes
  * @returns {JSX.Element} <Layout/>
  */
-const Layout = () => {
-  return (
-    <div class="md:flex md:min-h-screen">
-      <div class="md:w-1/4 ">1</div>
-      <div class="md:w-3/4">
-        <Outlet />
-      </div>
+const Layout = () => (
+  <div className="md:flex md:min-h-screen">
+    <div className="md:w-1/4 bg-gray-200 px-5 py-10">
+      <h2 className="text-3xl text-center text-gray-500">CRM - Clientes</h2>
+      <nav className="mt-10">
+        <a
+          className="text-gray-600 text-1xl block uppercase mt-2 hover:text-gray-900 transition duration-150 ease-out hover:ease-in"
+          href="/clientes"
+        >
+          Clientes
+        </a>
+        <a
+          className="text-gray-600 text-1xl block uppercase mt-2 hover:text-gray-900 transition duration-150 ease-out hover:ease-in"
+          href="/clientes/nuevo"
+        >
+          Nuevo Cliente
+        </a>
+      </nav>
     </div>
-  );
-};
-
+    <div className="md:w-3/4">
+      <Outlet />
+    </div>
+  </div>
+);
 export default Layout;
