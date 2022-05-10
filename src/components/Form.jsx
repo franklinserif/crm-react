d8a4e21f11ba975a2ebeb55fa1dd18a8563210a2
franklin rodriguez <franklinserif@gmail.com>
2022-05-10 19:27:38 -0400

    wrappe Form in anonymous function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,74 +16,76 @@ const Form = () => {
           note: '',
         }}
       >
-        <FormFormik className="mt-5">
-          <div className="mb-4">
-            <label htmlFor="name" className="text-gray-600">
-              Nombre
-            </label>
-            <Field
-              name="name"
-              id="name"
-              type="text"
-              placeholder="Nombre del cliente"
-              className="mt-2 block w-full p-3 bg-gray-50"
+        {() => (
+          <FormFormik className="mt-5">
+            <div className="mb-4">
+              <label htmlFor="name" className="text-gray-600">
+                Nombre
+              </label>
+              <Field
+                name="name"
+                id="name"
+                type="text"
+                placeholder="Nombre del cliente"
+                className="mt-2 block w-full p-3 bg-gray-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="company" className="text-gray-600">
+                Empresa
+              </label>
+              <Field
+                name="company"
+                id="company"
+                type="text"
+                placeholder="Empresa del cliente"
+                className="mt-2 block w-full p-3 bg-gray-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="text-gray-600">
+                Email
+              </label>
+              <Field
+                name="email"
+                id="email"
+                type="email"
+                placeholder="Email del cliente"
+                className="mt-2 block w-full p-3 bg-gray-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="phone" className="text-gray-600">
+                Teléfono
+              </label>
+              <Field
+                name="phone"
+                id="phone"
+                type="text"
+                placeholder="Teléfono del cliente"
+                className="mt-2 block w-full p-3 bg-gray-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="note" className="text-gray-600">
+                Notas
+              </label>
+              <Field
+                name="note"
+                as="textarea"
+                id="note"
+                type="text"
+                placeholder="Notas del cliente"
+                className="mt-2 block w-full p-3 bg-gray-50"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Agregar clientes"
+              className="mt-5 w-full bg-blue-500 p-3 text-white uppercase hover:bg-blue-600 transition cursor-pointer"
             />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="company" className="text-gray-600">
-              Empresa
-            </label>
-            <Field
-              name="company"
-              id="company"
-              type="text"
-              placeholder="Empresa del cliente"
-              className="mt-2 block w-full p-3 bg-gray-50"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="text-gray-600">
-              Email
-            </label>
-            <Field
-              name="email"
-              id="email"
-              type="email"
-              placeholder="Email del cliente"
-              className="mt-2 block w-full p-3 bg-gray-50"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="phone" className="text-gray-600">
-              Teléfono
-            </label>
-            <Field
-              name="phone"
-              id="phone"
-              type="text"
-              placeholder="Teléfono del cliente"
-              className="mt-2 block w-full p-3 bg-gray-50"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="note" className="text-gray-600">
-              Notas
-            </label>
-            <Field
-              name="note"
-              as="textarea"
-              id="note"
-              type="text"
-              placeholder="Notas del cliente"
-              className="mt-2 block w-full p-3 bg-gray-50"
-            />
-          </div>
-          <input
-            type="submit"
-            value="Agregar clientes"
-            className="mt-5 w-full bg-blue-500 p-3 text-white uppercase hover:bg-blue-600 transition cursor-pointer"
-          />
-        </FormFormik>
+          </FormFormik>
+        )}
       </Formik>
     </div>
   );
